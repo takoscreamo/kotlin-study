1f3dfbb4cc0fe24f3841de3f77f5f01f3766ddde
koiwa <ikanohiza@gmail.com>
2025-07-20 21:37:33 +0900

    implementing-server-side-kotlin-development Spring Initilizar でひな型を作成

---
+++ implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementing_server_side_kotlin_development/ImplementingServerSideKotlinDevelopmentApplication.kt
@@ -0,0 +1,11 @@
+package com.example.implementing_server_side_kotlin_development
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImplementingServerSideKotlinDevelopmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImplementingServerSideKotlinDevelopmentApplication>(*args)
+}
---
+++ implementing-server-side-kotlin-development/src/test/kotlin/com/example/implementing_server_side_kotlin_development/ImplementingServerSideKotlinDevelopmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.implementing_server_side_kotlin_development
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImplementingServerSideKotlinDevelopmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
