ad617a06cc935f1a065a05b8b831a3da2132ad37
koiwa <ikanohiza@gmail.com>
2025-07-24 16:57:03 +0900

    SwaggerUIがブラウザで開けなかったのでSpring Bootのバージョンを3.2.5にダウングレード

--- implementing-server-side-kotlin-development/build.gradle.kts
+++ implementing-server-side-kotlin-development/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
-	id("org.springframework.boot") version "3.5.3"
+	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.7"
 
 	/**
